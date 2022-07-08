7c28af990f895c0e2f3e97b9a8d6a81c8ba779e0
raqsanta <eahisla@gmail.com>
2022-07-08 02:07:54 -0300

    Add 404

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,6 @@
 
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
@@ -13,41 +14,58 @@ export default function Navbar() {
 
                     </div>
 
-                    <div className={styles.item_holder + ' nav-item'}>
-                        <Image className={styles.item_icon} src="/icons/home.png" width="32" height="32" />
-                        <br />
-                        Início
+                    <div className='d-flex'>
+
+                        <Link href="/main">
+                            <div className={styles.item_holder + ' mx-lg-3 mx-md-1 mx-0 nav-item'}>
+                                <Image className={styles.item_icon} src="/icons/home.png" width="32" height="32" />
+                                <br />
+                                <span>Início</span>
+                            </div>
+                        </Link>
+
+                        <Link href="/connections">
+                            <div className={styles.item_holder + ' mx-lg-3 mx-md-1 mx-0 nav-item'}>
+                                <Image className={styles.item_icon} src="/icons/person.png" width="32" height="32" />
+                                <br />
+                                <span>Conexões</span>
+                            </div>
+                        </Link>
+
+                        <Link href="/pro">
+                            <div className={styles.item_holder + ' mx-lg-3 mx-md-1 mx-0 nav-item'}>
+                                <Image className={styles.item_icon} src="/icons/briefcase.png" width="32" height="32" />
+                                <br />
+                                Seja Pro
+                            </div>
+                        </Link>
+
+                        <Link href="/comments">
+                            <div className={styles.item_holder + ' mx-lg-3 mx-md-1 mx-0 nav-item'}>
+                                <Image className={styles.item_icon} src="/icons/balloon.png" width="32" height="32" />
+                                <br />
+                                Comentários
+                            </div>
+                        </Link>
+
+                        <Link href="/comments">
+                            <div className={styles.item_holder + ' mx-lg-3 mx-md-1 mx-0 nav-item'}>
+                                <Image className={styles.item_icon} src="/icons/bell.png" width="32" height="32" />
+                                <br />
+                                Notificações
+                            </div>
+                        </Link>
+
+                        <Link href="/profile">
+                            <div className={styles.item_holder + ' mx-lg-3 mx-md-1 mx-0 nav-item'}>
+                                <Image className={styles.item_icon} src="/icons/profile.png" width="32" height="32" />
+                                <br />
+                                Seu Perfil
+                            </div>
+                        </Link>
+
                     </div>
 
-                    <div className={styles.item_holder + ' nav-item'}>
-                        <Image className={styles.item_icon} src="/icons/person.png" width="32" height="32" />
-                        <br />
-                        Conexões
-                    </div>
-
-                    <div className={styles.item_holder + ' nav-item'}>
-                        <Image className={styles.item_icon} src="/icons/briefcase.png" width="32" height="32" />
-                        <br />
-                        Premium
-                    </div>
-
-                    <div className={styles.item_holder + ' nav-item'}>
-                        <Image className={styles.item_icon} src="/icons/balloon.png" width="32" height="32" />
-                        <br />
-                        Comentários
-                    </div>
-
-                    <div className={styles.item_holder + ' nav-item'}>
-                        <Image className={styles.item_icon} src="/icons/bell.png" width="32" height="32" />
-                        <br />
-                        Notificações
-                    </div>
-
-                    <div className={styles.item_holder + ' nav-item'}>
-                        <Image className={styles.item_icon} src="/icons/profile.png" width="32" height="32" />
-                        <br />
-                        Seu Perfil
-                    </div>
 
                 </div>
             </nav>
---
+++ pages/404.js
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion"
+import styles from '../styles/Four.module.css'
+
+export default function FourOFour() {
+
+    return (
+
+        <motion.div
+            initial={{ opacity: 0, width: "100%" }}
+            animate={{ opacity: "100%" }}
+            exit={{ width: 0 }}
+        >
+
+            <main className={styles.main}>
+
+                <div className={styles.breathing_item}>
+                    <span className={styles.big_text}>
+                        404
+                    </span>
+                    <br />
+                    <span>
+                        Página não encontrada
+                    </span>
+                </div>
+
+
+            </main>
+
+        </motion.div>
+
+    )
+
+}
