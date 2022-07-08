67b19c036097f5118aa412b3bc5973db4ec20c2f
raqsanta <eahisla@gmail.com>
2022-07-08 01:11:37 -0300

    Update navbar

--- components/navbar.js
+++ components/navbar.js
@@ -7,16 +7,48 @@ export default function Navbar() {
         <div>
             <nav className="navbar bg-light shadow-sm d-none d-md-block">
                 <div className="container-fluid">
-                    <div className={styles.centered_bar+" navbar-brand"} href="#">
-                        <Image width="64" height="64" id='logo-image' className={styles.navbar_brand+' d-inline-block align-text-top'} src="/jobee.png" alt="Jobee Logo" />
-                        <div className={styles.item_spacing}>Barra de Pesquisa Aqui</div>
+                    <div className={styles.centered_bar + " navbar-brand"} href="#">
+                        <Image width="64" height="64" id='logo-image' className={styles.navbar_brand + ' d-inline-block align-text-top'} src="/jobee.png" alt="Jobee Logo" />
+                        <input className={styles.item_spacing + " form-control"} id="search-bar" placeholder="Tente buscar algo" />
+
                     </div>
-                    Home /
-                    Pessoas /
-                    Suitcase /
-                    Chat /
-                    Sino /
-                    Perfil /
+
+                    <div className={styles.item_holder + ' nav-item'}>
+                        <Image className={styles.item_icon} src="/icons/home.png" width="32" height="32" />
+                        <br />
+                        Início
+                    </div>
+
+                    <div className={styles.item_holder + ' nav-item'}>
+                        <Image className={styles.item_icon} src="/icons/person.png" width="32" height="32" />
+                        <br />
+                        Conexões
+                    </div>
+
+                    <div className={styles.item_holder + ' nav-item'}>
+                        <Image className={styles.item_icon} src="/icons/briefcase.png" width="32" height="32" />
+                        <br />
+                        Premium
+                    </div>
+
+                    <div className={styles.item_holder + ' nav-item'}>
+                        <Image className={styles.item_icon} src="/icons/balloon.png" width="32" height="32" />
+                        <br />
+                        Comentários
+                    </div>
+
+                    <div className={styles.item_holder + ' nav-item'}>
+                        <Image className={styles.item_icon} src="/icons/bell.png" width="32" height="32" />
+                        <br />
+                        Notificações
+                    </div>
+
+                    <div className={styles.item_holder + ' nav-item'}>
+                        <Image className={styles.item_icon} src="/icons/profile.png" width="32" height="32" />
+                        <br />
+                        Seu Perfil
+                    </div>
+
                 </div>
             </nav>
         </div>
